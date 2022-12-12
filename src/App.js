eaa26c4518739d86587d2da5369a32e6c88d8d85
John G <johngouldman@pm.me>
2022-12-12 17:15:00 -0600

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+
 
 function App() {
+const mode = 
+
+
   return (
     <div className="app">
-      
+      <Routes>
+      <Route path="/" element={<LoginPage/>} />
+      <Route path="/home" element={<HomePage/>} />
+      <Route path="/profile/:userId" element={<ProfilePage/>} />
+      </Routes>
+      <BrowserRouter/>
     </div>
   );
 }
